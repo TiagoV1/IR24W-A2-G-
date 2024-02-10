7b27a76293dc21f29f56be3cb86444dc1f2ba289
Queensley Lim <queensleylim@Queensleys-Air.home>
2024-02-10 00:46:27 -0800

    Added logic on finding and counting subdomains. Implemented create_subdomain_dictionary and edited is_ics_uci_edu_subdomain

--- TempForGroup/QueenyTemp.py
+++ TempForGroup/QueenyTemp.py
@@ -1,5 +1,8 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse
+from bs4 import BeautifulSoup
+import time
+from collections import namedtuple
 from urllib import robotparser
 
 
@@ -7,9 +10,35 @@ visited_urls = []                        # List of all urls that have been visit
 check_dynamic_traps_query = set()        # Set of sliced querys to check for dynamic traps
 date_terms = set("day", "month", "year") # Set of date terms
 
+
+global stop_words
+stop_words = ('a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', "aren't",
+              'as', 'at', 'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by',
+              "can't", 'cannot', 'could', "couldn't", 'did', "didn't", 'do', 'does', "doesn't", 'doing', "don't",
+              'down', 'during', 'each', 'few', 'for', 'from', 'further', 'had', "hadn't", 'has', "hasn't", 'have',
+              "haven't", 'having', 'he', "he'd", "he'll", "he's", 'her', 'here', "here's", 'hers', 'herself', 'him',
+              'himself', 'his', 'how', "how's", 'i', "i'd", "i'll", "i'm", "i've", 'if', 'in', 'into', 'is', "isn't",
+              'it', "it's", 'its', 'itself', "let's", 'me', 'more', 'most', "mustn't", 'my', 'myself', 'no', 'nor',
+              'not', 'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out',
+              'over', 'own', 'same', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'so', 'some',
+              'such', 'than', 'that', "that's", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there',
+              "there's", 'these', 'they', "they'd", "they'll", "they're", "they've", 'this', 'those', 'through', 'to',
+              'too', 'under', 'until', 'up', 'very', 'was', "wasn't", 'we', "we'd", "we'll", "we're", "we've", 'were',
+              "weren't", 'what', "what's", 'when', "when's", 'where', "where's", 'which', 'while', 'who', "who's",
+              'whom', 'why', "why's", 'with', "won't", 'would', "wouldn't", 'you', "you'd", "you'll", "you're",
+              "you've", 'your', 'yours', 'yourself', 'yourselves')
+#question 1 and question 2
+unique_pages_found = dict()#http://www.ics.uci.edu#aaa and http://www.ics.uci.edu#bbb are the same URL. # link='', word_count=0
+#question 3
+words_and_frequency = dict()
+#question 4
+subdomain_and_numpages = dict() # subdomain as key, pages count as value
+
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
+
 
 def extract_next_links(url, resp):
     # Implementation required.
@@ -21,7 +50,82 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    extracted_links = set()
+    if resp.status == 200 and resp.raw_response.content:
+            page_content = BeautifulSoup(resp.raw_response.content,'html.parser').get_text()
+            page_tokens = my_tokenize(page_content)
+            if len(page_tokens) > 100:
+                if is_ics_uci_edu_subdomain(url):
+                    create_subdomain_dictionary(url)
+
+                update_word_frequency(page_tokens)
+
+                for link in BeautifulSoup(resp.raw_response.content, 'html.parser').find_all('a', href=True):
+                    absolute_link = link['href']
+                    if absolute_link != url and absolute_link not in visited_urls:
+                        visited_urls.append(absolute_link)
+                        update_unique_pages_found(url, len(page_tokens))
+                        extracted_links.add(absolute_link)
+    else:
+        print("ERROR", resp.error)
+    time.sleep(2)
+    return list(extracted_links)
+
+
+def my_tokenize(text_content):
+    # this is Santiago's tokenize for assingmnet1 modefied to work for this assignment
+    # Tokens are all alphanumeric characters
+    token_list = list()
+    for word in re.findall('[^a-zA-Z0-9]', text_content):
+        if word.lower() not in stop_words:
+            token_list.append(word.lower())
+    return token_list
+
+
+def is_ics_uci_edu_subdomain(link):
+    # this function will help answer questions 4 and 1
+    # it checks if the subdomain is ics.uci.edu
+    hostInfo = urlparse(link).hostname
+    return re.match(r'(?:http?://|https?://).*\.ics\.uci\.edu', hostInfo)
+    
+    
+def create_subdomain_dictionary(url):
+    '''
+    Parses the URL and checks to see if the hostname 
+    is in the subdomain dictionary. If so, then the count
+    for the subdomain increases count by 1, else, makes it a 
+    key and sets it to 1.
+    '''
+    global subdomain_and_numpages
+    parsed_hostname = urlparse(url).hostname
+    if parsed_hostname in subdomain_and_numpages:
+        subdomain_and_numpages[parsed_hostname] += 1
+    else:
+        subdomain_and_numpages[parsed_hostname] = 1
+
+
+def update_word_frequency(tokens):
+    global words_and_frequency
+    for token in tokens:
+        if token not in words_and_frequency:
+            words_and_frequency[token] = 1
+        else:
+            words_and_frequency[token] += 1
+
+
+def update_unique_pages_found(link, other_word_count):
+    link = remove_fragment(link)
+    if link in unique_pages_found:
+        unique_pages_found[link] += other_word_count
+    else:
+        unique_pages_found[link] = other_word_count
+
+
+def remove_fragment(url):
+    parsed_url = urlparse(url)
+    url_without_fragment = parsed_url._replace(fragment='')
+    reconstructed_url = urlunparse(url_without_fragment)
+    return reconstructed_url
 
 
 def read_robots(url,  user_agent='*'):
@@ -42,7 +146,7 @@ def dynamic_trap_check(url, parsed):
     ''' 
     url_query = parsed.query
     index = url_query.index("=")
-    new_url_query = parsed.scheme + "://" + parsed.netloc + parsed.path + "?" + url_query[0:index]  # Rebuilds the url but with only the first parameter
+    new_url_query = parsed.hostname + parsed.path + "?" + url_query[0:index]  # Rebuilds the url but with only the first parameter
 
     if new_url_query in check_dynamic_traps_query:              # Checks if the URL is already been crawled through
         return True
@@ -55,7 +159,7 @@ def calendar_trap_check(parsed, path_segments):
     '''
     Checks for any URLs that are calendar traps.
     '''
-    date_pattern = re.compile(r'/(?:(?:\d{4}-\d{2}-\d{2})|(?:\d{4}-\d{2})|(?:\d{2}-\d{2}-\d{4})|(?:\d{1,2}/\d{1,2}/\d{2,4}))/')
+    date_pattern = re.compile(r'/(?:(?:\d{2,4}-\d{2}-\d{2,4})|(?:\d{2,4}-\d{2,4})|(?:\d{1,2}/\d{1,2}/\d{2,4}))/')
 
     if re.match(date_pattern, parsed.path) or bool(set(path_segments) & date_terms):    # Check if there is a number date format in the URL
         return True
@@ -88,7 +192,6 @@ def is_trap(url, parsed):
 
     return dynamic_trap_check(url, parsed) or calendar_trap_check(parsed, path_segments)  # Covers Dynamic URL Trap by checking for duplicate params
                                                                                           # and Covers Calendar Trap by checking repeating paths
-
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
